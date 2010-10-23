e2ebb75ae3e959c6c789b1eefeae30850a8f3144
Mike Gehard & Prakash Murthy <pair+mikegehard+_prakash@promiscuouspairprogramming.com>
2010-10-23 10:50:45 -0600

    Get devise, omniauth and rspec integrated

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '3.0.1'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'devise', '1.1.3'
+gem 'omniauth', '0.1.5'  
 
 # Use unicorn as the web server
 # gem 'unicorn'
@@ -30,7 +32,7 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 # end
 
 
-group :test do
+group :test, :development do
   gem 'rspec-rails'
   gem 'cucumber-rails'
   # use capybara instead of webrat
--- config/application.rb
+++ config/application.rb
@@ -34,9 +34,17 @@ module PromiscuousPairProgramming
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    config.encoding                          = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters                 += [:password]
+
+    config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+
   end
 end
