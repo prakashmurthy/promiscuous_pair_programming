bb1948babe952045eaa03910f5583041d085b3ef
Mike Gehard & Prakash Murthy <pair+mikegehard+_prakash@promiscuouspairprogramming.com>
2010-10-23 16:38:04 -0600

    Add description validation.

--- app/models/pairing_session.rb
+++ app/models/pairing_session.rb
@@ -1,3 +1,4 @@
 class PairingSession < ActiveRecord::Base
+  validates :description, :presence => true
   belongs_to :owner, :class_name => "User"
 end
--- spec/models/pairing_session_spec.rb
+++ spec/models/pairing_session_spec.rb
@@ -1,20 +1,42 @@
 require 'spec_helper'
 
 describe PairingSession do
-  subject {Factory.build(:pairing_session)}
+  subject { Factory.build(:pairing_session) }
 
   it "should have a valid factory" do
     subject.should be_valid
   end
 
-  it "should have an owner" do
-    email = "hello@world.com"
-    owner = Factory.build(:user, :email => email)
-    subject.owner = owner
-    subject.save!
-    subject.reload
+  describe "associations" do
+    it "should have an owner" do
+      email         = "hello@world.com"
+      owner         = Factory.build(:user, :email => email)
+      subject.owner = owner
+      subject.save!
+      subject.reload
 
-    subject.owner.email.should == email
+      subject.owner.email.should == email
 
+    end
+  end
+
+  describe "validations" do
+    it "should require a description" do
+      subject.description = nil
+
+      subject.should_not be_valid
+
+      subject.description = ""
+
+      subject.should_not be_valid
+    end
+
+#    it "should require a valid date" do
+#      subject.meeting_date = "hello"
+#      subject.should_not be_valid
+#
+#      subject.meeting_date = "12/40/2010"
+#      subject.should_not be_valid
+#    end
   end
 end
