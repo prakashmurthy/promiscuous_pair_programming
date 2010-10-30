0aa6721c1862636b29c27b707f886927f07c51ad
Patrick Reagan <reaganpr@gmail.com>
2010-10-29 23:33:45 -0600

    Implement pending date-releated specs; Refactor specs & implementation

--- app/models/pairing_session.rb
+++ app/models/pairing_session.rb
@@ -3,28 +3,33 @@ class PairingSession < ActiveRecord::Base
   belongs_to :owner, :class_name => "User"
 
   validates :description, :start_at, :end_at, :presence => true
-  
+
   validate :starts_in_future, :if => :timestamps_set?
   validate :ends_after_start_time, :if => :timestamps_set?
-  validate :no_overlap_between_sessions  
-  
+  validate :no_overlap_between_sessions, :if => :timestamps_set?
+
   private
-  
+
   def starts_in_future
     errors.add(:start_at, "must be in the future") if start_at < Time.now
   end
-  
+
   def ends_after_start_time
     errors.add(:end_at, "must be after the start time") if end_at <= start_at
   end
-  
+
   def timestamps_set?
     start_at && end_at
   end
-  
+
   def no_overlap_between_sessions
-    count = owner.pairing_sessions.count(:conditions => {:start_at => start_at, :end_at => end_at})
-    errors.add(:base, "You have already posted a session that overlaps with this one") if count > 0
+    errors.add(:base, "You have already posted a session that overlaps with this one") if overlapping_sessions?
   end
-  
+
+  def overlapping_sessions?
+    owner.pairing_sessions.count(:conditions => [
+      "(start_at <= ? AND end_at >= ?) OR (end_at >= ? AND start_at <= ?)", end_at, start_at, start_at, end_at
+    ]) > 0
+  end
+
 end
--- spec/models/pairing_session_spec.rb
+++ spec/models/pairing_session_spec.rb
@@ -6,7 +6,7 @@ describe PairingSession do
   it "should have a valid factory" do
     subject.should be_valid
   end
-  
+
   describe "associations" do
     it "should have an owner" do
       email         = "hello@world.com"
@@ -14,72 +14,120 @@ describe PairingSession do
       subject.owner = owner
       subject.save!
       subject.reload
-  
+
       subject.owner.email.should == email
-  
+
     end
   end
-  
+
   describe "validations" do
     it "should require a description" do
       subject.description = nil
-  
+
       subject.should_not be_valid
-  
+
       subject.description = ""
-  
+
       subject.should_not be_valid
     end
-    
+
     it "must be in the future" do
       subject.start_at = 1.day.ago
       subject.should_not be_valid
     end
-    
+
     it "requires the start time" do
       subject.start_at = nil
       subject.should_not be_valid
     end
-    
+
     it "requires the end time" do
       subject.end_at = nil
       subject.should_not be_valid
     end
-    
+
     it "requires a start time occurring before the end time" do
       subject.start_at = 2.day.from_now
       subject.end_at = 1.day.from_now
       subject.should_not be_valid
     end
-    
-    it "cannot overlap with an existing request" do
-      start_at = 1.day.from_now
-      end_at   = 2.days.from_now
-      
-      user    = Factory.create(:user)
-      request = Factory.create(:pairing_session, :owner => user, :start_at => start_at, :end_at => end_at)
 
-      overlapping_request = Factory.build(:pairing_session, :owner => user, :start_at => start_at, :end_at => end_at)
-      overlapping_request.should_not be_valid
-    end
-    
-    it "considers sessions within a start / end window to be overlapping"
-    it "considers sessions with an end time after the start time of another session to be overlapping"
-    it "considers sessions with a start time before an end time of another session to be overlapping"
-    
-    it "does not consider a request from another user as overlapping" do
-      start_at = 1.day.from_now
-      end_at   = 2.days.from_now
-      
-      user1    = Factory.create(:user)
-      user2    = Factory.create(:user)
-      request = Factory.create(:pairing_session, :owner => user1, :start_at => start_at, :end_at => end_at)
+    context "with an existing pairing session" do
+      before do
+        start_at = 1.day.from_now
+        end_at   = 2.days.from_now
 
-      overlapping_request = Factory.build(:pairing_session, :owner => user2, :start_at => start_at, :end_at => end_at)
-      overlapping_request.should be_valid
+        @user    = Factory.create(:user)
+        @session = Factory.create(:pairing_session, :owner => @user)
+      end
+
+      it "knows that a session after the existing one does not overlap" do
+        other_session = Factory.build(:pairing_session, {
+          :owner    => @user,
+          :start_at => (@session.end_at + 1.second),
+          :end_at   => (@session.end_at + 2.seconds)
+
+        })
+        other_session.should be_valid
+      end
+
+      it "knows that a session before the existing one does not overlap" do
+        other_request = Factory.build(:pairing_session, {
+          :owner    => @user,
+          :start_at => (@session.start_at - 2.seconds),
+          :end_at   => (@session.start_at - 1.second)
+
+        })
+        other_request.should be_valid
+      end
+
+      it "knows that another session with the same times is overlapping" do
+        overlapping_session = Factory.build(:pairing_session, {
+          :owner    => @user,
+          :start_at => @session.start_at,
+          :end_at   => @session.end_at
+        })
+        overlapping_session.should_not be_valid
+      end
+
+      it "knows that a session inside the window of an existing session is overlapping" do
+        overlapping_session = Factory.build(:pairing_session, {
+          :owner    => @user,
+          :start_at => @session.start_at + 1.second,
+          :end_at   => @session.end_at - 1.second
+        })
+        overlapping_session.should_not be_valid
+      end
+
+      it "considers sessions with an end time after the start time of another session to be overlapping" do
+        overlapping_session = Factory.build(:pairing_session, {
+          :owner    => @user,
+          :start_at => @session.start_at - 2.seconds,
+          :end_at   => @session.start_at + 1.second
+        })
+        overlapping_session.should_not be_valid
+      end
+
+      it "considers sessions with a start time before an end time of another session to be overlapping" do
+        overlapping_session = Factory.build(:pairing_session, {
+          :owner    => @user,
+          :start_at => @session.end_at - 1.second,
+          :end_at   => @session.end_at + 2.seconds
+        })
+        overlapping_session.should_not be_valid
+      end
+
+      it "does not consider a session from another user as overlapping" do
+        other_user    = Factory.create(:user)
+        other_session = Factory.build(:pairing_session, {
+          :owner    => other_user,
+          :start_at => @session.start_at,
+          :end_at   => @session.end_at
+        })
+        other_session.should be_valid
+      end
     end
-    
-      
+
 
 #    it "should require a valid date" do
 #      subject.meeting_date = "hello"
