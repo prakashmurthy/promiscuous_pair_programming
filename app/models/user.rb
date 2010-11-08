cf71859b26d8d951aed012227749147676c369ee
Mike Gehard <mike.gehard@gmail.com>
2010-11-07 17:18:15 -0700

    Added first name and last name to the user#new and user#edit forms so users can now add a required first and last name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable
 
-#  validates :first_name, :last_name, :presence => true
+  validates :first_name, :last_name, :presence => true
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation#, :remember_me
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation#, :remember_me
 
   has_many :owned_pairing_sessions, :class_name => "PairingSession", :foreign_key => :owner_id
   has_many :pairing_sessions_as_pair, :class_name => "PairingSession", :foreign_key => :pair_id
--- features/registration.feature
+++ features/registration.feature
@@ -6,6 +6,8 @@ Feature: Registering new user for the site
   Scenario: Creating a user account
     When I go to the root page
     And I follow "Create an account" within the account management section
+    And I fill in "First name" with "Bob"
+    And I fill in "Last name" with "Test"
     And I fill in "Email" with "bob@test.com"
     And I fill in "Password" with "my_password"
     And I fill in "Password confirmation" with "my_password"
