f9d2d8feee8d3bc862caef948fe1d6aa066f5d5f
Elliot Winkler <elliot.winkler@gmail.com>
2010-11-09 19:10:32 -0700

    Tweak the Geolocation mixin to use ActiveSupport::Concern

--- features/step_definitions/geolocation_steps.rb
+++ features/step_definitions/geolocation_steps.rb
@@ -1,5 +1,6 @@
 # I know that nothing should be mocked in Cucumber, but this is a specific case,
 # since we're making an external API call.
+
 Given /^the location of the (?:new )?pairing session will be geolocated as "([^"]*)"$/ do |location|
   PairingSession.stub(:geolocation_disabled?) { false }
   PairingSession.stub(:fetch_location_info) {
--- lib/ppp/model_mixins/geolocation.rb
+++ lib/ppp/model_mixins/geolocation.rb
@@ -1,14 +1,12 @@
 module PPP
   module ModelMixins
     module Geolocation
+      extend ActiveSupport::Concern
+      
       GEOLOC_ATTRIBUTES = [
         :lat, :lng, :street_address, :city, :province, :district, :state, :zip, :country,
         :country_code, :accuracy, :precision, :suggested_bounds, :provider
       ]
-      
-      def self.included(base)
-        base.extend(ClassMethods)
-      end
       
       module ClassMethods
         def is_geolocatable
@@ -18,7 +16,6 @@ module PPP
           end
 
           attr_accessor :enable_geolocation
-          #def enable_geolocation=(value); @enable_geolocation = (value.to_s == "true"); end
           alias_method :enable_geolocation?, :enable_geolocation
           
           attr_accessor :raw_location
@@ -28,8 +25,6 @@ module PPP
           before_save :do_geolocation, :if => :do_geolocation?
           
           belongs_to :location, :autosave => true
-          #accepts_nested_attributes_for :location
-          #attr_accessible :location_attributes
           # Import methods that allow us to search by radius, etc.
           # Additionally, locations are actually stored in a separate table, but the :through
           # allows us to call User.find_within(50, ...) by reaching into the location association.
@@ -54,26 +49,28 @@ module PPP
         end
       end
       
-      # Show "Location can't be blank" instead of "Raw location can't be blank"
-      def read_attribute_for_validation(name)
-        name.to_s == "location" ? super("raw_location") : super(name)
-      end
-
-    private
-      def do_geolocation
-        if attrs = self.class.fetch_location_info(raw_location)
-          logger.debug "Got attributes:"
-          logger.debug attrs.inspect
-          location = new_record? ? self.build_location : self.location
-          location.attributes = attrs
-        else
-          self.errors.add_to_base("The location you entered couldn't be geolocated for some reason. You might try enter something simpler such as a zip code.")
+      module InstanceMethods
+        # Show "Location can't be blank" instead of "Raw location can't be blank"
+        def read_attribute_for_validation(name)
+          name.to_s == "location" ? super("raw_location") : super(name)
         end
-      end
 
-      def do_geolocation?
-        (!self.class.geolocation_disabled? && enable_geolocation?).tap do |value|
-          logger.debug "Geolocation enabled? #{value.inspect}"
+      private
+        def do_geolocation
+          if attrs = self.class.fetch_location_info(raw_location)
+            logger.debug "Got attributes:"
+            logger.debug attrs.inspect
+            location = new_record? ? self.build_location : self.location
+            location.attributes = attrs
+          else
+            self.errors.add_to_base("The location you entered couldn't be geolocated for some reason. You might try enter something simpler such as a zip code.")
+          end
+        end
+
+        def do_geolocation?
+          (!self.class.geolocation_disabled? && enable_geolocation?).tap do |value|
+            logger.debug "Geolocation enabled? #{value.inspect}"
+          end
         end
       end
     end
