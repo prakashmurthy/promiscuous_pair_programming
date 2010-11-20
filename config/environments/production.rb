a6502c32f4ac03e2dff3b843a4ec748af938b5eb
msgehard & prakashmurthy <pair+mikegehard+_prakash@promiscuouspairprogramming.com>
2010-11-20 15:28:00 -0700

    Change application name for production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-PromiscuousPairProgramming::Application.configure do
+PPP::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   # The production environment is meant for finished, "live" apps.
