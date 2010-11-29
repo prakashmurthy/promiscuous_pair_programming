137c0a552e97a6d4dc4c5853b27b4f02615924ae
Mike Gehard <mike.gehard@gmail.com>
2010-11-29 13:31:27 -0700

    Add in debugging step to see the email queue.

--- features/step_definitions/debugging_steps.rb
+++ features/step_definitions/debugging_steps.rb
@@ -1,3 +1,9 @@
 When /^I debug$/ do
   debugger
 end
+When /^show me the mail queue$/ do
+  puts "Here is the mail queue:"
+  ActionMailer::Base.deliveries.each do |mail|
+    puts mail.inspect
+  end
+end
